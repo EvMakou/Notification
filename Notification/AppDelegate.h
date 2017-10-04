44fcb7a14957a032ebba6dcb30a095718ef24af6
supermacho <face_buk@mail.ru>
2017-10-04 12:23:25 +0300

    Initial Commit

---
+++ Notification/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Notification
+//
+//  Created by supermacho on 04.10.17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
